1c68a1335c639084acec16d334d2d73b83d1f5d0
shaylavi <shaylavister@gmail.com>
2019-06-20 01:42:29 +1000

    first commit - arduino based joystick for unity

---
+++ Assets/Arduino.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System;
+using UnityEngine.UI;
+
+public class Arduino : MonoBehaviour
+{
+
+    public Text externalTextResource;
+    private string[] arduinoOutput = new string[3];
+
+    SerialPort stream;
+
+    private int x = 0;
+    private int y = 0;
+    private bool press = false;
+    private bool status = false;
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public bool getPress()
+    {
+        return this.press;
+    }
+    public bool getStatus()
+    {
+        return this.status;
+    }
+
+
+    void Start()
+    {
+        openStream(true);
+    }
+
+    private void OnApplicationQuit()
+    {
+        this.status = false;
+        if (stream != null && stream.IsOpen)
+            stream.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            openStream(true);
+            if (stream != null && stream.IsOpen)
+            {
+                status = true;
+                arduinoOutput = stream.ReadLine().Split(',');
+                stream.BaseStream.Flush();
+                x = Int16.Parse(arduinoOutput[0]);
+                y = Int16.Parse(arduinoOutput[1]);
+                press = Int16.Parse(arduinoOutput[2]) == 0 ? false : true;
+
+                updateText();
+            }
+            else
+            {
+                openStream(false);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error caught: " + e);
+        }
+    }
+
+    private bool openStream(bool open)
+    {
+        try
+        {
+            if (open)
+            {
+                if (stream == null)
+                {
+                    stream = new SerialPort("COM3", 9600);
+                }
+                else if (stream != null && !stream.IsOpen)
+                {
+                    stream.ReadTimeout = 1000;
+                    stream.Open();
+                    status = true;
+                }
+
+                return true;
+            }
+            else
+            {
+                if (stream != null && stream.IsOpen)
+                    stream.Close();
+
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error caught: " + e);
+            return false;
+        }
+    }
+    private void updateText()
+    {
+        if (externalTextResource != null && externalTextResource.isActiveAndEnabled)
+            externalTextResource.text = string.Format("X Value: {0}\r\nY Value: {1}\r\nButton: {2}", x, y, press);
+    }
+
+
+}
---
+++ Assets/MovementControl.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+    Vector3 loc;
+    public bool useArduino = false;
+    public Arduino arduinoScript;
+    private int x = 0;
+    private int y = 0;
+    private bool press = false;
+    private const int X_ANKER = 503;
+    private const int Y_ANKER = 534;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loc = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementSpeed = 0.3f;
+
+        if (!useArduino)
+        {
+            if (Input.GetKey("w"))
+            {
+                loc.y += movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (Input.GetKey("s"))
+            {
+                loc.y -= movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (Input.GetKey("a"))
+            {
+                loc.x -= movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (Input.GetKey("d"))
+            {
+                loc.x += movementSpeed;
+                gameObject.transform.position = loc;
+            }
+        }
+        else
+        {
+            this.x = arduinoScript.getX() - X_ANKER;
+            this.y = arduinoScript.getY() - Y_ANKER;
+            this.press = arduinoScript.getPress();
+
+            if (this.x < 0 && this.x < -1)
+            {
+                movementSpeed += (float)Mathf.Abs(this.x) / X_ANKER;
+
+                loc.x -= movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (this.x > 0)
+            {
+                movementSpeed += (float)Mathf.Abs(this.x) / X_ANKER;
+
+                loc.x += movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (this.y < 0)
+            {
+                movementSpeed += (float)Mathf.Abs(this.y) / Y_ANKER;
+
+                loc.y -= movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (this.y > 0)
+            {
+                movementSpeed += (float)Mathf.Abs(this.y) / Y_ANKER;
+
+                loc.y += movementSpeed;
+                gameObject.transform.position = loc;
+            }
+            if (this.press)
+            {
+                Debug.Log("Button fired..");
+            }
+
+        }
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+    public int getY()
+    {
+        return this.y;
+    }
+
+}
